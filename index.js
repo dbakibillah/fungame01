5057e9854ba14ab776684b38eceb01cde909ad86
bakibillah <bakibillah.dev@gmail.com>
2024-09-28 03:09:35 +0600

    tic tac toe added

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 document.getElementById("btn-rock-paper-scissors").addEventListener("click", () => {
     window.location.href = "./rock_paper_scissors/home.html";
-})
+});
+
+document.getElementById("btn-tic-tac-toe").addEventListener("click", () => {
+    window.location.href = "./tic_tac_toe/ticTacToe.html";
+});
